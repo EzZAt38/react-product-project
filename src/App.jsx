f5f05e67707533c685a4daf7e08cb34847bf0388
EzZAt38 <r25068210@gmail.com>
2025-06-13 08:15:49 -0400

    add Edait  modal

--- src/App.jsx
+++ src/App.jsx
@@ -27,10 +27,12 @@ const initialErrorState = {
 const App = () => {
   // Modal visibility state
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isEdit, setIsEdit] = useState(false);
   const [AddProduct, setAddProduct] = useState(productList);
   const [selected, setSelected] = useState(categories[0]);
   // Product form state
   const [product, setProduct] = useState(initialProductState);
+  console.log(product);
   //temp colors state
   const [tempColors, setTempColors] = useState([]);
   // Error messages for form fields
@@ -77,11 +79,17 @@ const App = () => {
       onCancel();
     }
   };
-  console.log(tempColors);
   // Helper for input change
   const handleInputChange = (field) => (e) => {
     setProduct({ ...product, [field]: e.target.value });
-    setErrors({ ...errors, [field]: "" }); // Clear error on change
+    const colorVaild = tempColors.length !== 0;
+    if (!colorVaild) {
+      setErrors({
+        ...errors,
+        [field]: "",
+        colors: "at least select one color",
+      }); // Clear error on change
+    }
   };
 
   return (
@@ -99,22 +107,97 @@ const App = () => {
             price={prod.price}
             imageURL={prod.imageURL}
             color={prod.colors}
+            category={prod.category}
+            setProduct={setProduct}
+            isopen={()=> setIsEdit(true)}
           >
             {/* Edit and Destroy Buttons for each product */}
-            <ButtonComponent
-              color={styles.submit}
-              text="Edit"
-              onClick={openModal}
-            />
-            <ButtonComponent color={styles.delete} text="Destroy" />
           </ProductCard>
         ))}
       </div>
 
-      {/* Modal for Adding/Editing Product */}
+      {/* Modal for Adding Product */}
+      <MyModal isOpen={isModalOpen} closeModal={closeModal} title="Add Product">
+        <form className="mt-2 flex flex-col space-y-4" onSubmit={handlerSubmit}>
+          {/* Product Name Input */}
+          <div>
+            <Input
+              id="product-name"
+              label="Product name:"
+              placeholder="Write your product name here"
+              value={product.title}
+              onChange={handleInputChange("title")}
+            />
+            <ErorrMessage message={errors.title} />
+          </div>
+          {/* Product Image URL Input */}
+          <div>
+            <Input
+              id="product-image"
+              label="Product Image URL:"
+              placeholder="Paste your product image URL here"
+              value={product.imageURL}
+              onChange={handleInputChange("imageURL")}
+            />
+            <ErorrMessage message={errors.imageURL} />
+          </div>
+          {/* Description Input */}
+          <div>
+            <Input
+              id="description"
+              label="Description:"
+              placeholder="Write your description here"
+              value={product.description}
+              onChange={handleInputChange("description")}
+            />
+            <ErorrMessage message={errors.description} />
+          </div>
+          {/* Price Input */}
+          <div>
+            <Input
+              id="price"
+              label="Price:"
+              placeholder="Write your price here"
+              value={product.price}
+              onChange={handleInputChange("price")}
+            />
+            <ErorrMessage message={errors.price} />
+          </div>
+          {/* category side  */}
+          <Example selected={selected} setSelcted={setSelected} />
+          {/* circle side */}
+          <ErorrMessage message={errors.colors} />
+          <div className="flex items-center space-x-4 ">{MapColors}</div>
+          <div className="grid grid-cols-4 gap-1">
+            {tempColors.map((color, index) => (
+              <span
+                key={index}
+                className={`p-1 text-center text-white rounded-md`}
+                style={{ backgroundColor: color }}
+              >
+                {color}
+              </span>
+            ))}
+          </div>
+          {/* Modal Action Buttons */}
+          <div className="mt-4 flex space-x-1">
+            <ButtonComponent
+              color={styles.submit}
+              text="Submit"
+              width="w-full"
+            />
+            <ButtonComponent
+              color={styles.delete}
+              text="Delete"
+              onClick={onCancel}
+            />
+          </div>
+        </form>
+      </MyModal>
+      {/* Modal for Editing Product */}
       <MyModal
-        isOpen={isModalOpen}
-        closeModal={closeModal}
+        isOpen={isEdit}
+        closeModal={() => setIsEdit(false)}
         title="Edit Product"
       >
         <form className="mt-2 flex flex-col space-y-4" onSubmit={handlerSubmit}>
@@ -165,6 +248,7 @@ const App = () => {
           {/* category side  */}
           <Example selected={selected} setSelcted={setSelected} />
           {/* circle side */}
+          <ErorrMessage message={errors.colors} />
           <div className="flex items-center space-x-4 ">{MapColors}</div>
           <div className="grid grid-cols-4 gap-1">
             {tempColors.map((color, index) => (
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -3,14 +3,26 @@ import ButtonComponent from "../ui/ButtonComponent";
 import { Icard, styles, textLenther } from "../interfaces/interface";
 import Image from "./Image";
 import CircleColor from "./CircleColor";
+interface Iprops extends Icard {
+  setProduct: (value:Icard) => void; // Optional edit function
+isopen:()=> void; 
+}
 const ProductCard = ({
   imageURL,
   description,
   price,
   title,
   children,
-  color=[],
-}: Icard) => {
+  color = [],
+  category,
+  setProduct,
+  isopen
+}: Iprops) => {
+  const product = { imageURL, description, price, title, color, category };
+  const onEdit = () => {
+  setProduct(product);
+  isopen();
+  };
   const [textState, setTextState] = useState(true);
   return (
     <div className={styles.icardparents}>
@@ -34,23 +46,35 @@ const ProductCard = ({
               : textLenther(description, description.length)}
           </p>
           <div className="colorSide flex items-center space-x-1">
-            {color.map((colorItem, index) => (<CircleColor color={colorItem} key={index}/>))}
+            {color.map((colorItem, index) => (
+              <CircleColor color={colorItem} key={index} />
+            ))}
           </div>
         </div>
       </div>
       {/* this is the footer side */}
       {/* price and category side */}
       <div className={styles.categoryAndPrice}>
-        <p className="text-[#A31ACB]  font-bold">${price}</p>
-        <Image
-          imageURL={imageURL}
-          alt="product image"
-          className="rounded-full w-10 h-10"
-        />
+        <p className="text-indigo-600  font-bold">${price}</p>
+        <div className="flex items-center space-x-2 px-2">
+          <p className="font-semibold">{category?.name}</p>
+          <Image
+            imageURL={category?.imageURL || ""}
+            alt="product image"
+            className="rounded-full w-10 h-10"
+          />
+        </div>
       </div>
       {/* price and category side */}
       {/* this is the footer side */}
-      <div className="footer flex  space-x-2 p-1">{children}</div>
+      <div className="footer flex  space-x-2 p-1">
+                   <ButtonComponent
+                      color={styles.submit}
+                      text="Edit"
+                      onClick={onEdit}
+                    />
+                    <ButtonComponent color={styles.delete} text="Destroy" />
+      </div>
     </div>
   );
 };
